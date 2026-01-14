19e2806feb0a49256d7dd8222b27cae88425a933
omshantiom <er.ankit3107@gmail.com>
2026-01-14 16:13:56 +0530

    added new screen UI
    added new screen UI

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import App from "./App";
+import "./src/ScreensUI/SplashScreen";
+import "./src/ScreensUI/GetAQuote";
+import "./src/ScreensUI/HomeScreen";
+import "./src/ScreensUI/AddBenefits";
+import "./src/ScreensUI/LifeInsuranceScreen";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
